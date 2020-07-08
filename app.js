975ed3bd658833bb2172fc2b1a35330ca759f254
orabxd <orabxd@yahoo.com>
2020-07-08 16:14:50 -0600

    Responsive v1.5

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
     document.querySelector(".nav-menu").classList.toggle("show");
-  });
-  
-  ScrollReveal().reveal('.showcase');
-  ScrollReveal().reveal('.help-cards', { delay: 500 });
+});
+
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".help-cards", { delay: 500 });
+
+
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+                left: '-50%'
+			});
+		}
+	});
+ 
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
